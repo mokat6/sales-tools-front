df59c790275f0bd2ff86aa02ed4fbf2a67f6ba4b
Momo <github2@5208866.xyz>
2025-07-16 21:56:10 +0300

    fixed theme select - actually use the hook

--- src/components/ThemeSelect.tsx
+++ src/components/ThemeSelect.tsx
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
 import { Select } from "./Select";
+import { useTheme, type Theme } from "@/context/theme/theme-context";
 
-const themeOptions = [
+type optionType = {
+  label: string;
+  value: Theme;
+};
+
+const themeOptions: optionType[] = [
   { label: "Light", value: "light" },
   { label: "Dark", value: "dark" },
   { label: "Green", value: "green" },
 ];
 
 export function ThemeSelect() {
-  const [theme, setTheme] = useState(() => {
-    const storedTheme = localStorage.getItem("theme");
-    console.log("up in Paris: ", storedTheme);
-    if (storedTheme) return storedTheme;
-
-    const isSystemDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    return isSystemDark ? "dark" : "light";
-  });
-
-  useEffect(() => {
-    document.documentElement.className = "";
-    document.documentElement.className = theme;
-    localStorage.setItem("theme", theme);
-  }, [theme]);
+  const { setTheme, theme } = useTheme();
 
   return <Select options={themeOptions} value={theme} onValueChange={setTheme} />;
 }
