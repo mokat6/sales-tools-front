c7d90c0c7bca2980d1706af489a0d89515eb04ed
Momo <github2@5208866.xyz>
2025-07-19 08:36:56 +0300

    sideMenu unified, single

--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,22 +1,15 @@
-import Sidebar from "./Sidebar";
+import SideMenu from "./SideMenu";
 import { Route, Routes } from "react-router-dom";
 import ViewBigData from "./ViewBigData";
 import AddCompanyScreen from "./AddCompanyScreen";
 import Settings from "./Settings";
 import Crm from "./Crm";
-import { MenuIcon } from "lucide-react";
-import { useState } from "react";
 
 export default function App() {
-  const [sideMenuOpen, setSideMenuOpen] = useState(true);
-
   return (
     <div className="flex h-screen overflow-hidden">
-      <SideMenuBase className="absolute">
-        <MenuIcon size={29} onClick={() => setSideMenuOpen(!sideMenuOpen)} className="cursor-pointer" />
-      </SideMenuBase>
+      <SideMenu />
 
-      {sideMenuOpen && <Sidebar Base={SideMenuBase} />}
       <div className="flex-1 overflow-auto bg-bg-background">
         <Routes>
           <Route path="/" element={<ViewBigData />} />
@@ -32,8 +25,4 @@ export default function App() {
 export type SideMenuBaseProps = {
   children: React.ReactNode;
   className: string;
-};
-
-const SideMenuBase = ({ className, children }: SideMenuBaseProps) => {
-  return <div className={`flex w-12 bg-amber-400 flex-col items-center pt-4 gap-4 ${className}`}>{children}</div>;
 };
